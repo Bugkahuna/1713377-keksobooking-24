6a5f2c120d303f053b06082a68fe87853fd9306f
Vladimir Kochevanov <v.b.kochevanov@gmail.com>
2021-10-01 20:01:18 +0300

    Убрал console. Добавил 2 функции возвращающие целое и число с плавающей точкой

--- js/main.js
+++ js/main.js
@@ -3,8 +3,7 @@
 function getIntNumber(min, max){
   return (max-min > 0)&&(min >=0) ? Math.floor(Math.random()*(max-min+1))+min: 'Неверно указан диапазон значений';
 }
-
-console.log(getIntNumber(2, 100));
+getIntNumber(0, 10);
 
 
 //Функция возвращает число с плавающей точкой из заданного диапазона, включая крайние значения.
@@ -13,4 +12,4 @@ function getFloatNumber(min, max){
   return (max-min > 0)&&(min >=0) ? Math.random()*(max-min+1)+min: 'Неверно указан диапазон значений';
 }
 
-console.log(getFloatNumber(0, 0));
+getFloatNumber(-5, 63);
